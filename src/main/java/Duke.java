f7e7a75a1cbb10ca172b03716df92091e84dfb2e
Haocheeks <e1525305@u.nus.edu>
2026-08-29 22:01:11 +0800

    Set up the Launcher and Duke Class for JavaFX

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,19 @@
-public class Duke {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Duke extends Application{
     public static void main(String[] args) {
         System.out.println("Hello!");
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our scene
+        stage.show(); // Render the stage.
     }
 }
